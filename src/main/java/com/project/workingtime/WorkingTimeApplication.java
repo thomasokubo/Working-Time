942a74917038bc4e12d188adc8018ab4998fee27
Thomas Okubo <tai.okubo93@gmail.com>
2018-03-08 14:08:31 -0300

    Create project with Spring Boot

---
+++ src/main/java/com/project/workingtime/WorkingTimeApplication.java
@@ -0,0 +1,12 @@
+package com.project.workingtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingTimeApplication.class, args);
+	}
+}
