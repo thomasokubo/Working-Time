fdd2f54cfbea1f98a1851be9cc39856c73c531ef
Thomas Okubo <tai.okubo93@gmail.com>
2018-04-09 14:08:13 -0300

    Add createdTime field

--- src/main/java/com/project/workingtime/repository/Checker.java
+++ src/main/java/com/project/workingtime/repository/Checker.java
@@ -1,10 +1,7 @@
 package com.project.workingtime.repository;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 import static com.project.workingtime.utils.DateTimeUtils.localDateTimeToString;
@@ -13,20 +10,25 @@ import static com.project.workingtime.utils.DateTimeUtils.localDateTimeToString;
 public class Checker {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
 
+    @Column(name = "check_in")
     private String checkin;
 
+    @Column(name = "check_out")
     private String checkout;
 
+    @Column(name = "created_time", columnDefinition = "timestamp")
+    private LocalDateTime createdTime;
 
     public Checker() {
-        checkin = localDateTimeToString(LocalDateTime.now());
+        LocalDateTime current = LocalDateTime.now();
+        checkin = localDateTimeToString(current);
         checkout = "";
-
+        createdTime = current;
     }
-
 
     public String getCheckin() {
         return checkin;
